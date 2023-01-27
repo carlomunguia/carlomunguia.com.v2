3995e7d6702846fdf678988d54d129ae2e500db2
Carlo Munguia <cmunguia@denimgroup.com>
2023-01-27 00:40:48 -0600

    added project/resume/music folders/routes & began navbar creation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,45 @@
-<h1 class="text-center text-4xl">Welcome to Carlo Munguia Land</h1>
+<script lang="ts">
+	export let menuButton = true;
+
+	function handleClick() {
+		menuButton = !menuButton;
+	}
+</script>
+
+<div class="navbar bg-base-100">
+	<div class="flex-none">
+		<button class="btn btn-square btn-ghost">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				class="inline-block w-5 h-5 stroke-current"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M4 6h16M4 12h16M4 18h16"
+				/></svg
+			>
+		</button>
+	</div>
+	<div class="flex-1">
+		<button on:click={handleClick} class="btn btn-ghost normal-case text-xl">Carlo Munguia</button>
+	</div>
+	<div class="flex-none">
+		<button class="btn btn-square btn-ghost">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				class="inline-block w-5 h-5 stroke-current"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+				/></svg
+			>
+		</button>
+	</div>
+</div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,3 @@
+<h1>About My Site</h1>
+
+<a href="/">Home</a>
